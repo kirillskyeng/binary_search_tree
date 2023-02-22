08db713e0d6f965cce5c614e8f553b1afbee5339
kirillskyeng <kirill.netreba@skyeng.ru>
2023-02-22 19:20:36 +0300

    Add Node class and tests.

---
+++ src/node.py
@@ -0,0 +1,8 @@
+class Node:
+    """Класс для представления узла с данными."""
+
+    def __init__(self, data: dict) -> None:
+        """Узел инициализируется словарем с данными."""
+        self.data = data
+        self.left = None
+        self.right = None
