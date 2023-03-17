1458d2ba28b71c07495c204d55d6422ad9d28622
kirillskyeng <kirill.netreba@skyeng.ru>
2023-03-17 20:16:56 +0300

    Add insert full realization. 100% coverage

--- src/binary_search_tree.py
+++ src/binary_search_tree.py
@@ -1,12 +1,13 @@
 from src.node import Node
 
+
 class BinarySearchTree:
     def __init__(self) -> None:
         """Инициализируем всегда пустое бинарное деверов поиска."""
         self.__root = None
 
     @property
-    def root(self):
+    def root(self) -> Node | None:
         """Возвращает ссылку на вершину BST."""
         return self.__root
 
@@ -14,6 +15,27 @@ class BinarySearchTree:
         """Добавляем данные в структуру согласно правилам BST."""
         if self.__root is None:
             self.__root = Node(data)
+        else:
+            self._insert_recursively(self.__root, data)
+
+    def _insert_recursively(self, node: Node, data: dict) -> None:
+        """Рекурсивно ищет место для вставки новых данных."""
+        if data['id'] < node.data['id']:
+            if node.left is None:
+                node.left = Node(data)
+            else:
+                self._insert_recursively(node.left, data)
+
+        if data['id'] > node.data['id']:
+            if node.right is None:
+                node.right = Node(data)
+            else:
+                self._insert_recursively(node.right, data)
+
+
+
+
+
 
     def search(self, post_id: int) -> dict:
         """Ищем и возвращаем словарь с данными о посте по его id."""
--- tests/test_binary_search_tree.py
+++ tests/test_binary_search_tree.py
@@ -1,4 +1,14 @@
+import pytest
+
 from src.binary_search_tree import BinarySearchTree
+
+
+@pytest.fixture
+def bst_root():
+    """Создаем BST с одним узлом в вершине."""
+    bst = BinarySearchTree()
+    bst.insert({'id': 40})
+    return bst
 
 
 def test_bst_empty():
@@ -7,9 +17,32 @@ def test_bst_empty():
     assert bst.root is None
 
 
-def test_bst_root():
+def test_bst_root(bst_root):
     """Добавляем данные в вершину BST."""
-    bst = BinarySearchTree()
-    data = {'id': 40}
-    bst.insert(data)
-    assert bst.root.data == data
+    assert bst_root.root.data == {'id': 40}
+
+
+def test_bst_insert_left(bst_root):
+    """Добавляем данные в лево от вершины."""
+    bst_root.insert({'id': 30})
+    assert bst_root.root.left.data['id'] == 30
+
+
+def test_bst_insert_right(bst_root):
+    """Добавляем данные в лево от вершины."""
+    bst_root.insert({'id': 50})
+    assert bst_root.root.right.data['id'] == 50
+
+
+def test_bst_insert_left_left(bst_root):
+    """Добавляем данные в лево от вершины два раза."""
+    bst_root.insert({'id': 30})
+    bst_root.insert({'id': 25})
+    assert bst_root.root.left.left.data['id'] == 25
+
+
+def test_bst_insert_right_right(bst_root):
+    """Добавляем данные в лево от вершины."""
+    bst_root.insert({'id': 50})
+    bst_root.insert({'id': 60})
+    assert bst_root.root.right.right.data['id'] == 60
